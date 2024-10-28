0ad09dac15603bcce3f6841fc5189610f3a8561f
andreireporter13 <andrei.reporter13@gmail.com>
2024-10-28 22:41:03 +0200

    added headers cors data fixed

--- core/settings.py
+++ core/settings.py
@@ -50,14 +50,13 @@ CORS_ALLOWED_ORIGINS = [
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
