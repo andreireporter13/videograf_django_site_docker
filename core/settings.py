53d030ec587ebb91b301cf725f52f98feab81b92
andreireporter13 <andrei.reporter13@gmail.com>
2025-01-05 19:08:05 +0200

    added little changes

--- core/settings.py
+++ core/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$fd!-hx4pnkghmi#2c9*x1d8eg26&u#b@@_a8cwz(7$eaj+#pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['81.180.202.188', 'razvanvideo.ro', 'www.razvanvideo.ro']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['81.180.202.188', 'razvanvideo.ro', 'www.razvanvideo.ro']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
